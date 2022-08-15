525421a225ef51ca41920b962e0dadde46eaa88a
salviega <salviega6@gmail.com>
2022-08-15 08:19:01 -0500

    [FEAT] Contratos desplegados

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,19 @@
-require('@nomiclabs/hardhat-waffle')
-require('dotenv').config()
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 /**
  @type import('hardhat/config').HardhatUserConfig
 **/
 
 module.exports = {
-  paths: { // se enrutan las fuentes del blockchain
+  paths: {
+    // se enrutan las fuentes del blockchain
     sources: './src/blockchain/hardhat/contracts',
     tests: './src/blockchain/hardhat/test',
     cache: './src/blockchain/hardhat/cache',
-    artifacts: './src/blockchain/hardhat/artifacts'
+    artifacts: './src/blockchain/hardhat/artifacts',
   },
-  defaultNetwork: 'kovan',
+  defaultNetwork: 'rinkeby',
   networks: {
     hardhat: {
       // If want to do some forking, uncomment this
@@ -20,13 +21,12 @@ module.exports = {
       //  url: MAINNET_RPC_URL
       // }
     },
-    localhost: {
-    },
-    kovan: {
-      url: process.env.KOVAN_RPC_URL,
+    localhost: {},
+    rinkeby: {
+      url: process.env.RINKEBY_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
-      saveDeployments: true
-    }
+      saveDeployments: true,
+    },
   },
-  solidity: '0.8.9'
-}
+  solidity: '0.8.9',
+};
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { ethers } from 'ethers';
 
-import addresess from '../../blockchain/environment/contract-address.json'
-import nftDinamicCompile from '../../blockchain/hardhat/artifacts/src/blockchain/hardhat/contracts/NFTDinamic.sol/NFTDinamic.json'
+import addresess from '../../blockchain/environment/contract-address.json';
+import nftDinamicCompile from '../../blockchain/hardhat/artifacts/src/blockchain/hardhat/contracts/NFTDinamic.sol/NFTDinamic.json';
+import nftManualCompile from '../../blockchain/hardhat/artifacts/src/blockchain/hardhat/contracts/NFTManual.sol/NFTManual.json';
 
 function App() {
+  const nft = async () => {
+    const provider = new ethers.providers.JsonRpcProvider('https://rinkeby.infura.io/v3/b28b2656eeb34d4c8af86c3ca0ea3bc2');
+    const nftManualContract = new ethers.Contract(addresess.nftmanualcontract, nftManualCompile.abi, provider);
+    const tokenUri = await nftManualContract.tokenURI(0);
+  };
 
-  const nft = () => {
-    const provider = ethers.providers.getDefaultProvider('localhost')
-    const nftDinamicContract = new ethers.Contract(addresess.nftdinamiccontract, nftDinamicCompile.abi, provider)
-
-
-  }
-
-  React.useEffect(()=> {
+  React.useEffect(() => {
     nft();
-  }, [])
+  }, []);
 
   return (
     <React.Fragment>
